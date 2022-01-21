b89c329e7fb68238a7b3361013ea217c93ca7521
Hamza-ben-yaflah <benyaflah0hamza@gmail.com>
2022-01-21 16:41:56 +0100

    delete and edit product

--- src/Pages/ProductPage/Popup.tsx
+++ src/Pages/ProductPage/Popup.tsx
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 import { Form, Input, InputNumber, Button } from "antd";
 import "./Popup.css";
 import { DataType } from "./ProductPage";
+import form from "antd/lib/form";
 interface PopupProp {
   handleClose: () => void;
-  handleSubmit: (data: DataType) => void;
+  handleSubmit: (data: DataType, isEditMode: boolean) => void;
+  product: DataType | undefined;
 }
 
-function Popup({ handleClose, handleSubmit }: PopupProp) {
+function Popup({ handleClose, handleSubmit, product }: PopupProp) {
+  const [form] = Form.useForm();
+
   const onSubmit = (values: DataType) => {
-    handleSubmit(values);
+    const editMode = product ? true : false;
+    handleSubmit(values, editMode);
+    form.resetFields();
     handleClose();
   };
 
@@ -19,26 +25,26 @@ function Popup({ handleClose, handleSubmit }: PopupProp) {
         <button className="close-btn" onClick={handleClose}>
           x
         </button>
-        <Form onFinish={onSubmit}>
+        <Form onFinish={onSubmit} initialValues={product}>
           <Form.Item name="key" label="Id" rules={[{ required: true }]}>
             <InputNumber />
           </Form.Item>
           <Form.Item name="name" label="Name" rules={[{ required: true }]}>
-            <Input />
+            <Input value={product?.name} />
           </Form.Item>
           <Form.Item
             name="quanlity"
             label="Quanlity"
             rules={[{ required: true }]}
           >
-            <InputNumber />
+            <InputNumber value={product?.quanlity} />
           </Form.Item>
           <Form.Item name="price" label="price" rules={[{ required: true }]}>
-            <InputNumber />
+            <InputNumber value={product?.price} />
           </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit">
-              Add Product
+              {product ? <span>Edit </span> : <span>Add </span>} Product
             </Button>
           </Form.Item>
         </Form>
--- src/Pages/ProductPage/ProductPage.tsx
+++ src/Pages/ProductPage/ProductPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Table, Layout, Button } from "antd";
+import { Table, Layout, Button, Space } from "antd";
 import Popup from "./Popup";
 
 export interface DataType {
@@ -9,38 +9,77 @@ export interface DataType {
   price: number;
 }
 
-const columns = [
-  {
-    title: "Id",
-    dataIndex: "key",
-  },
-
-  {
-    title: "Name",
-    dataIndex: "name",
-    render: (text: string) => <a>{text}</a>,
-  },
-  {
-    title: "Quanlity",
-    dataIndex: "quanlity",
-  },
-  {
-    title: "Price",
-    dataIndex: "price",
-  },
-];
-
 const { Header } = Layout;
 
 function ProductPage() {
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "key",
+    },
+
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (text: string) => <a>{text}</a>,
+    },
+    {
+      title: "Quanlity",
+      dataIndex: "quanlity",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text: string, record: DataType) => (
+        <Space size="middle">
+          <Button
+            type="primary"
+            className="btn"
+            onClick={() => {
+              setIsOpen(true);
+              setProductToEdit(record);
+            }}
+          >
+            Edit
+          </Button>
+          <Button
+            type="primary"
+            className="btn"
+            onClick={() => deleteProduct(record)}
+          >
+            Delete
+          </Button>
+        </Space>
+      ),
+    },
+  ];
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [products, setProducts] = useState<DataType[]>([]);
+  const [productToEdit, setProductToEdit] = useState<DataType | undefined>();
 
-  const addProduct = (newProduct: DataType) =>
-    setProducts([...products, newProduct]);
+  const addProduct = (newProduct: DataType, isEditMode: boolean) => {
+    isEditMode
+      ? setProducts(
+          products.map((p) => {
+            if (p.key === newProduct.key) return newProduct;
+
+            return p;
+          })
+        )
+      : setProducts([...products, newProduct]);
+  };
+
+  const deleteProduct = (product: DataType) => {
+    setProducts(products.filter((p) => p.key !== product.key));
+  };
 
   const togglePopup = () => {
     setIsOpen(!isOpen);
+    setProductToEdit(undefined);
   };
 
   return (
@@ -51,7 +90,13 @@ function ProductPage() {
         </Button>
       </Header>
       <Table columns={columns} dataSource={products} />
-      {isOpen && <Popup handleClose={togglePopup} handleSubmit={addProduct} />}
+      {isOpen && (
+        <Popup
+          handleClose={togglePopup}
+          handleSubmit={addProduct}
+          product={productToEdit}
+        />
+      )}
     </div>
   );
 }
